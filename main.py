6079ece7a850fd56294a8f7a931574c63ba545a9
avila <1angelovila@gmail.com>
2016-12-23 01:20:11 -0800

    cleanup main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from urllib_personal import page_crawl_pick
 
 
 regular_expression = '<li><a href="/pdf/web/viewer\.html\?file=(.+\.pdf)" target="_blank">(.+)</a><br>\r\n.+G\.R\. No\. (\d+)\. (.+)</li>'
+## will generate 
+## pdf link - ('/jurisprudence/2016/february2016/204970.pdf',
+## prosecutor and defense - 'Spouses Claudio and Carmencita Trayvilla Vs. Bernardo Sejas and Juvy Paglinawan represented by Jessie Paglinawan',
+## G R NO - '204970',
+## Date - 'February 1, 2016')
+
 url = 'http://sc.judiciary.gov.ph/jurisprudence/2016/toc/february.php'
 
 
